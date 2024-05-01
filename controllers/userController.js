720bed96a036611b3bfe783781fe44187c3321e2
Thomas-Studstrup <thst23ad@student.cbs.dk>
2024-05-01 11:23:00 +0200

    User_Reg_VIRKER

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const asyncHandler = require("../middlewares/asyncHandler");
 const registerUser = asyncHandler(async (req, res, next) => {
   console.log(req.body.Username, req.body.Password)
   console.log(req.body)
-    let user = new User(req.body.Username, req.body.Password);
+    let user = new User(req.body.Username, req.body.Password, req.body.Email, req.body.Age, req.body.Weight, req.body.Gender);
     let registerUser = user.registerUser();
 
 
