b01623bdf5df49d1e982a1a9e53ec2e4d1113dae
Thomas-Studstrup <thst23ad@student.cbs.dk>
2024-05-07 12:16:20 +0200

    GeoLocation

--- controllers/mealTrackerController.js
+++ controllers/mealTrackerController.js
@@ -1,14 +1,24 @@
 const MealTracker = require("../models/MealTracker");
 const asyncHandler = require("../middlewares/asyncHandler");
 
-const trackMeal = asyncHandler(async (req, res) => {
-    const { MealID, DateTime, Amount } = req.body;
-    const UserID = req.session.user.userID;
-    console.log('Session user:', req.session.user + 'body ' + { MealID, DateTime, Amount }); // Log session user
-    await MealTracker.trackMeal(UserID, MealID, DateTime, Amount);
-    res.status(200).json({ success: true, message: "Meal tracked" });
+// const trackMeal = asyncHandler(async (req, res) => {
+//     const { MealID, DateTime, Amount } = req.body;
+//     const UserID = req.session.user.userID;
+//     console.log('Session user:', req.session.user + 'body ' + { MealID, DateTime, Amount }); // Log session user
+//     await MealTracker.trackMeal(UserID, MealID, DateTime, Amount);
+//     res.status(200).json({ success: true, message: "Meal tracked" });
 
-    });
+//     });
+
+const trackMeal = asyncHandler(async (req, res) => {
+  const { MealID, DateTime, Amount, latitude, longitude } = req.body;
+  const UserID = req.session.user.userID;
+  console.log('Session user:', req.session.user + 'body ' + { MealID, DateTime, Amount, latitude, longitude }); // Log session user and geolocation
+  await MealTracker.trackMeal(UserID, MealID, DateTime, Amount, latitude, longitude);
+  res.redirect('/mealtracker');
+});
+
+
 
 const getMeals = asyncHandler(async (req, res) => {
   console.log("1");
--- models/MealTracker.js
+++ models/MealTracker.js
@@ -2,18 +2,25 @@ const sql = require('mssql');
 const config = require('../config');
 const moment = require('moment-timezone');
 
-async function trackMeal(UserID, MealID, DateTime, Amount) {
+
+
+
+
+
+async function trackMeal(UserID, MealID, DateTime, Amount, latitude, longitude) {
     const adjustedDatetime = moment.utc(DateTime).add(2, 'hours').format('YYYY-MM-DD HH:mm:ss');
-    console.log('Meal logged:', { UserID, MealID, adjustedDatetime, Amount });
-    
+    console.log('Meal logged:', { UserID, MealID, adjustedDatetime, Amount, latitude, longitude });
+
     await sql.connect(config);
     await sql.query`
-        INSERT INTO Intake (UserID, MealID, DateTime, Amount)
-        VALUES (${UserID}, ${MealID}, ${adjustedDatetime}, ${Amount})`;
+        INSERT INTO Intake (UserID, MealID, DateTime, Amount, Location)
+        VALUES (${UserID}, ${MealID}, ${adjustedDatetime}, ${Amount}, ${latitude + ',' + longitude})`;
     console.log('Meal tracked successfully!');
 }
 
 module.exports.trackMeal = trackMeal;
+
+
 
 
 async function getMeals(UserID) {
